4b0234415e7bfa5c42786e11820a1888d7eaad1e
Yash Gupta <yashgupta0241@gmail.com>
2021-08-15 01:19:51 +0530

    Final Deployment 9

--- Banking/settings.py
+++ Banking/settings.py
@@ -119,9 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'bank\static')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 print(STATIC_ROOT)
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
