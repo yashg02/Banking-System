976e48013b9092cd26de624e9135116b08d57d6e
Yash Gupta <yashgupta0241@gmail.com>
2021-08-15 16:50:47 +0530

    Ho jaa bc 9

--- Banking/settings.py
+++ Banking/settings.py
@@ -124,7 +124,7 @@ PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 # print(STATIC_ROOT)
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static/")]
 
 # Managing media
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
