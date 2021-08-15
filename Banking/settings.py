f597626cfe75fb4579db28d2cb77c04c2b32bd1d
Yash Gupta <yashgupta0241@gmail.com>
2021-08-15 14:42:38 +0530

    Ho jaa bc 5

--- Banking/settings.py
+++ Banking/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'msz@)5-(w@vd$wmf54^lcjw0(bn&ms-cy=xabg1x!zl*#n2+yn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','bankfeatgrip.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
